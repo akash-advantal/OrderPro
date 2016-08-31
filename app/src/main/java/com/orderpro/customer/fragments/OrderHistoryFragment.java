9d636ef8cc27dfb42622091774cf111cb28b21e0
akash-advantal <akash.sisodia@advantal.net>
2016-08-31 16:26:21 +0530

    update 31-Aug-16

--- app/src/main/java/com/orderpro/customer/fragments/OrderHistoryFragment.java
+++ app/src/main/java/com/orderpro/customer/fragments/OrderHistoryFragment.java
@@ -1,18 +1,22 @@
 package com.orderpro.customer.fragments;
+
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.merchant.orderpro.R;
 import com.orderpro.customer.adapter.OrderHistoryAdapter;
 import com.orderpro.customer.bean.OrderHistorydata;
 import com.orderpro.customer.ui.NavigationActivity;
+import com.orderpro.customer.ui.OrderDetailsActivity;
 
 import java.util.ArrayList;
 
@@ -23,9 +27,7 @@ public class OrderHistoryFragment extends Fragment implements View.OnClickListen
     ListView order_listview;
     NavigationActivity navigationActivity;
     ArrayList<OrderHistorydata> orderlist;
-
-
-    Button drawer_icon;
+    ImageView drawer_icon;
     View view;
 
     @Nullable
@@ -33,7 +35,7 @@ public class OrderHistoryFragment extends Fragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.orderhistory_lv, container, false);
-        drawer_icon = (Button) view.findViewById(R.id.drawer_icon);
+        drawer_icon = (ImageView) view.findViewById(R.id.drawer_icon);
         order_listview = (ListView) view.findViewById(R.id.lv_orderhistory);
         drawer_icon.setOnClickListener(this);
 
@@ -44,7 +46,12 @@ public class OrderHistoryFragment extends Fragment implements View.OnClickListen
         orderlist.add(new OrderHistorydata("Elegant Blue Shirt", "Delivery Status - Pending", "Delivered by 31, july, 2016", R.drawable.ic_launcher));
         OrderHistoryAdapter md = new OrderHistoryAdapter(getActivity(), orderlist);
         order_listview.setAdapter(md);
-
+        order_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getActivity(), OrderDetailsActivity.class));
+            }
+        });
         return view;
     }
 
@@ -65,7 +72,6 @@ public class OrderHistoryFragment extends Fragment implements View.OnClickListen
                 break;
         }
     }
-
 
 
 
